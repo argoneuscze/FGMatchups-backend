bfc94231cf8fd1f9d8fd3eb9dd412bf11c52d261
argoneus <argoneuscze@gmail.com>
2017-11-29 13:26:43 +0100

    fixed seeding the db

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,11 +2,11 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    Rails.application.load_seed
   end
 
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
-      Rails.application.load_seed
       example.run
     end
   end
