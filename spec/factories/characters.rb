929bb2e9fcd2d1b79bceb279bfe533f43773478e
argoneus <argoneuscze@gmail.com>
2017-12-08 12:16:35 +0100

    made Character factory unique

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
+  sequence :short_name do |n|
+    "TE#{n}"
+  end
+
+  sequence :full_name do |n|
+    "Test Character #{n}"
+  end
+
   factory :character do
-    short_name { Faker::Lorem.characters(2) }
-    full_name { Faker::Lorem.sentence(1, false, 2) }
+    short_name { generate(:short_name) }
+    full_name { generate(:full_name) }
   end
 end
