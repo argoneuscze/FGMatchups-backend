6876b3914c989fda13eb26229bfffe547f10eaed
argoneus <argoneuscze@gmail.com>
2017-12-08 12:31:53 +0100

    added a new helper for invalid id

--- spec/requests/characters_spec.rb
+++ spec/requests/characters_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Characters API', type: :request do
   let!(:characters) { create_list(:character, 10) }
-  let(:char_id) { characters.first.id }
+  let(:valid_char_id) { characters.first.id }
+  let(:invalid_char_id) { valid_char_id + 10 }
 
   describe 'GET /characters' do
     before { get '/characters' }
