c348dd0bf2b37317dc7a124844d2d9999419a7db
argoneus <argoneuscze@gmail.com>
2017-12-08 17:25:28 +0100

    basic MatchupItem controller implemented

--- app/controllers/matchup_items_controller.rb
+++ app/controllers/matchup_items_controller.rb
@@ -1,2 +1,17 @@
 class MatchupItemsController < ApplicationController
+  before_action :set_matchup_item, only: [:show]
+
+  def index
+    json_response(MatchupItem.all)
+  end
+
+  def show
+    json_response(@matchup_item)
+  end
+
+  private
+
+  def set_matchup_item
+    @matchup_item = MatchupItem.find(params[:id])
+  end
 end
