5e7542c14107b13425de54744208a899b0ef9f9e
argoneus <argoneuscze@gmail.com>
2017-12-08 17:07:44 +0100

    implemented Matchup controller

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,2 +1,17 @@
 class MatchupsController < ApplicationController
+  before_action :set_matchup, only: [:show]
+
+  def index
+    json_response(Matchup.all)
+  end
+
+  def show
+    json_response(@matchup)
+  end
+
+  private
+
+  def set_matchup
+    @matchup = Matchup.find(params[:id])
+  end
 end
