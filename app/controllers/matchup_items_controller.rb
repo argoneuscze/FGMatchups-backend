c66eee59b67bbd0ea4164011dafd823df9f0cdb3
argoneus <argoneuscze@gmail.com>
2017-12-10 22:40:16 +0100

    added more methods to matchup items

--- app/controllers/matchup_items_controller.rb
+++ app/controllers/matchup_items_controller.rb
@@ -1,6 +1,6 @@
 class MatchupItemsController < ApplicationController
   before_action :set_matchup
-  before_action :set_matchup_item, only: [:show]
+  before_action :set_matchup_item, only: [:show, :update, :destroy]
 
   def index
     json_response(@matchup.matchup_items)
@@ -10,7 +10,25 @@ class MatchupItemsController < ApplicationController
     json_response(@matchup_item)
   end
 
+  def create
+    @matchup.matchup_items.create!(item_params)
+    json_response(@matchup, :created)
+  end
+
+  def update
+    @matchup_item.update!(item_params)
+    json_response(@matchup_item, :ok)
+  end
+
+  def destroy
+    @matchup_item.destroy!
+  end
+
   private
+
+  def item_params
+    params.permit(:description)
+  end
 
   def set_matchup
     @matchup = Matchup.find(params[:matchup_id])
--- spec/requests/matchup_items_spec.rb
+++ spec/requests/matchup_items_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Matchup Items API' do
   let(:invalid_matchup_id) { valid_matchup_id + 1 }
   let(:valid_item_id) { items.first.id }
   let(:invalid_item_id) { valid_item_id + 10 }
+  let(:valid_attrs) { { description: 'This is an item' } }
 
   describe 'GET /matchups/:matchup_id/matchup_items' do
     context 'when matchup exists' do
@@ -61,6 +62,73 @@ RSpec.describe 'Matchup Items API' do
         expect(json).not_to be_empty
         expect(json['message']).to match(/Couldn't find MatchupItem/)
       end
+    end
+  end
+
+  describe 'POST /matchups/:matchup_id/matchup_items' do
+    context 'when attributes are valid' do
+      before { post "/matchups/#{valid_matchup_id}/matchup_items", params: valid_attrs }
+
+      it 'adds the item' do
+        MatchupItem.find_by!(valid_attrs)
+      end
+
+      it 'returns 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when attributes are invalid' do
+      before { post "/matchups/#{valid_matchup_id}/matchup_items", params: {} }
+
+      it 'returns error message' do
+        expect(json).not_to be_empty
+        expect(json['message']).to match(/Validation failed/)
+      end
+
+      it 'returns 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'PATCH /matchups/:matchup_id/matchup_items/:matchup_item_id' do
+    context 'when the attributes are valid' do
+      before { patch "/matchups/#{valid_matchup_id}/matchup_items/#{valid_item_id}", params: valid_attrs }
+
+      it 'updates the item' do
+        item = MatchupItem.find(valid_item_id)
+        expect(item.description).to eq valid_attrs[:description]
+      end
+
+      it 'returns 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the attributes are invalid' do
+      before { patch "/matchups/#{valid_matchup_id}/matchup_items/#{valid_item_id}", params: { description: '' } }
+
+      it 'returns error message' do
+        expect(json).not_to be_empty
+        expect(json['message']).to match(/Validation failed/)
+      end
+
+      it 'returns 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'DELETE /matchups/:matchup_id/matchup_items/:matchup_item_id' do
+    before { delete "/matchups/#{valid_matchup_id}/matchup_items/#{valid_item_id}" }
+
+    it 'deletes the record' do
+      expect(MatchupItem.exists?(valid_item_id)).to be_falsey
+    end
+
+    it 'returns 204' do
+      expect(response).to have_http_status(204)
     end
   end
 end
